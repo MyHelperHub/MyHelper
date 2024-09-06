3fa5e8c416d77b567f2b7bf5557041b51e22f878
ialtone <1425592567@qq.com>
2024-09-06 23:48:48 +0800

    修改主页样式

--- src/pages/menu.vue
+++ src/pages/menu.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="menu-container">
     <div class="menu-list">
-      <div class="menu-item">常用网站</div>
-      <div class="menu-item">常用软件</div>
-      <div class="menu-item">桌面便签</div>
-      <div class="menu-item">定时计时器</div>
-      <div class="menu-item">CHATGPT</div>
-      <div class="menu-item">使用教程</div>
+      <div class="menu-item">
+        <div class="text">常用网站</div>
+      </div>
+      <div class="menu-item">
+        <div class="text">常用软件</div>
+      </div>
+      <div class="menu-item">
+        <div class="text">桌面便签</div>
+      </div>
+      <div class="menu-item">
+        <div class="text">定时计时器</div>
+      </div>
+      <div class="menu-item">
+        <div class="text">CHATGPT</div>
+      </div>
+      <div class="menu-item">
+        <div class="text">使用教程</div>
+      </div>
     </div>
   </div>
 </template>
@@ -31,26 +43,75 @@
     align-items: center;
 
     .menu-item {
-      width: 80%;
-      height: 30px;
-      margin: 13px 0;
-      padding-left: 5px;
-      display: flex;
-      align-items: center;
-      background-color: #9fc0cf;
       position: relative;
-      transition: background-color 0.2s ease-out;
+      display: flex;
+      justify-content: center;
+      width: 100%;
+      font-size: 14px;
+      font-weight: bold;
+      letter-spacing: 2px;
+      text-transform: uppercase;
+      transition: 0.3s;
+      color: rgb(28, 31, 30);
 
-      &:hover {
-        background-color: rgb(245, 245, 245);
-        z-index: 2;
-        /* 使用相对定位 */
-        top: 0;
-        left: 0;
-        /* 移除宽度和高度变化 */
-        height: 30px;
+      .text {
         width: 80%;
+        height: 30px;
+        margin: 13px 0;
+        padding-left: 5px;
+        display: flex;
+        align-items: center;
+        background-color: #9fc0cf;
+        position: relative;
         transition: background-color 0.2s ease-out;
+
+        &:hover {
+          background-color: rgb(245, 245, 245);
+          z-index: 2;
+          transition: background-color 0.2s ease-out;
+        }
+      }
+
+      /* 上半部分的竖线 */
+      &::before {
+        content: '';
+        width: 2px;
+        /* 竖线的宽度 */
+        height: 13px;
+        /* 初始高度为0 */
+        background-color: rgb(54, 56, 55);
+        /* 竖线的颜色 */
+        position: absolute;
+        left: 15px;
+        /* 定位在左侧 */
+        top: 20px;
+        /* 初始位置在中间 */
+        transition: height 0.2s ease-out, top 0.2s ease-out;
+      }
+
+      /* 下半部分的竖线 */
+      &::after {
+        content: '';
+        width: 2px;
+        /* 竖线的宽度 */
+        height: 13px;
+        /* 初始高度为0 */
+        background-color: rgb(54, 56, 55);
+        /* 竖线的颜色 */
+        position: absolute;
+        left: 15px;
+        /* 定位在左侧 */
+        bottom: 20px;
+        /* 初始位置在中间 */
+        transition: height 0.2s ease-out, bottom 0.2s ease-out;
+      }
+
+      &:hover::before {
+        height: 24px;
+      }
+
+      &:hover::after {
+        height: 24px;
       }
     }
   }
