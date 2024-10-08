35a83a821b4af610fead7358dbc9b0b24c727f95
ialtone <1425592567@qq.com>
2024-10-08 18:08:45 +0800

    快捷输入样式

--- src/views/quick-input/QuickInput.vue
+++ src/views/quick-input/QuickInput.vue
@@ -1,89 +1,96 @@
 <template>
-    <div class="quick-input">
-        <div class="tabs">
-            <div class="tab" :class="{ active: activeTab === 'common' }" @click="activeTab = 'common'">
-                常用
-            </div>
-            <div class="tab" :class="{ active: activeTab === 'clipboard' }" @click="activeTab = 'clipboard'">
-                剪切板
-            </div>
-        </div>
-        <div class="tab-content-wrapper">
-            <transition name="fade">
-                <div class="tab-content" v-if="activeTab === 'common'" key="common">
-                    <Common />
-                </div>
-            </transition>
-            <transition name="fade">
-                <div class="tab-content" v-if="activeTab === 'clipboard'" key="clipboard">
-                    <Clipboard />
-                </div>
-            </transition>
-        </div>
+  <div class="quick-input">
+    <div class="tabs">
+      <div
+        class="tab"
+        :class="{ active: activeTab === 0 }"
+        @click="activeTab = 0">
+        常用
+      </div>
+      <div
+        class="tab"
+        :class="{ active: activeTab === 1 }"
+        @click="activeTab = 1">
+        剪切板
+      </div>
     </div>
+    <div class="tab-content-wrapper">
+      <transition name="fade">
+        <div class="tab-content" v-if="activeTab === 0" key="0">
+          <Common />
+        </div>
+      </transition>
+      <transition name="fade">
+        <div class="tab-content" v-if="activeTab === 1" key="1">
+          <Clipboard />
+        </div>
+      </transition>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import Common from './Common.vue';
-import Clipboard from './Clipboard.vue';
+import { ref } from "vue";
+import Common from "./Common.vue";
+import Clipboard from "./Clipboard.vue";
 
-const activeTab = ref('common'); // 默认选项卡为常用
+/** 0为常用，1为剪切板 */
+const activeTab = ref(0);
 </script>
 
 <style lang="less">
 .quick-input {
-    position: absolute;
-    left: -13px;
-    background-color: rgb(242, 244, 253);
-    width: 210px;
-    min-height: 90px;
-    height: fit-content;
-    border-radius: 10px;
-    z-index: 2;
-    cursor: default;
+  position: absolute;
+  left: -13px;
+  background-color: rgb(242, 244, 253);
+  width: 210px;
+  min-height: 90px;
+  height: fit-content;
+  border-radius: 10px;
+  z-index: 2;
+  cursor: default;
 }
 
 .tabs {
-    font-size: 14px;
-    display: flex;
-    justify-content: space-between;
-    margin: 0 20px;
-    padding: 0 20px;
-    border-bottom: 1px solid #ccc;
+  font-size: 14px;
+  display: flex;
+  justify-content: space-between;
+  margin: 0 20px;
+  padding: 0 20px;
+  border-bottom: 1px solid #ccc;
 }
 
 .tab {
-    padding: 6px;
-    cursor: pointer;
+  padding: 6px;
+  cursor: pointer;
 }
 
 .tab.active {
-    font-weight: bold;
-    border-bottom: 2px solid #5295dd;
-    /* 选中的 tab 下方边框 */
+  font-weight: bold;
+  border-bottom: 2px solid #5295dd;
+  /* 选中的 tab 下方边框 */
 }
 
 .tab-content-wrapper {
-    position: relative;
-    height: fit-content;
+  position: relative;
+  height: fit-content;
 }
 
 .tab-content {
-    position: absolute;
-    width: 100%;
-    left: 0;
-    top: 0;
+  position: absolute;
+  width: 100%;
+  left: 0;
+  top: 0;
 }
 
 /* 动画效果 */
 .fade-enter-active,
 .fade-leave-active {
-    transition: opacity 0.3s ease;
+  transition: opacity 0.3s ease;
 }
 
 .fade-enter,
 .fade-leave-to {
-    opacity: 0;
+  opacity: 0;
 }
 </style>
