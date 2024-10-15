7fc82207d06a2b7387384c0cf5ae937071ec8c27
ialtone <1425592567@qq.com>
2024-10-15 23:41:25 +0800

    引入primevue

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,20 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/main.css";
+import "primeicons/primeicons.css";
 import { invoke } from "@tauri-apps/api/core";
 import { Window } from "@tauri-apps/api/window";
+import PrimeVue from "primevue/config";
+import Lara from "@primevue/themes/lara";
 if (Window.getCurrent().label === "main") {
   invoke("set_window_size", { width: 65, height: 65 });
 }
 
 const app = createApp(App);
 app.use(router);
+app.use(PrimeVue, {
+  theme: {
+    preset: Lara,
+  },
+});
 app.mount("#app");
