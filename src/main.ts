96d058a12417acbc5f000667a42f32a2cf9d853f
ialtone <1425592567@qq.com>
2024-10-24 23:53:08 +0800

    取消注释

--- src/main.ts
+++ src/main.ts
@@ -18,36 +18,36 @@ import { getConfig } from "./utils/config";
 import { listen } from "@tauri-apps/api/event";
 import Tooltip from "primevue/tooltip";
 
-// if (Window.getCurrent().label === "main") {
-//   ipcSetWindowSize(65, 65);
-//   async function initializeApp() {
-//     try {
-//       // 初始化设置
-//       initSetting();
-//       let settingData: Record<string, boolean> = await getConfig([
-//         "settingConfig",
-//       ]);
-//       if (!settingData) {
-//         settingData = {};
-//       }
-//       // 监听设置变化,将所有设置都放在main窗口里执行
-//       listen(
-//         "setting-change",
-//         (event: { payload: { key: string; value: boolean } }) => {
-//           const { key, value } = event.payload;
-//           handleSettingChange(key, value);
-//         },
-//       );
-//       await runStartupTasks((key) => {
-//         return settingData[key] === true;
-//       });
-//     } catch (error) {
-//       console.error("初始化失败:", error);
-//     }
-//   }
+if (Window.getCurrent().label === "main") {
+  ipcSetWindowSize(65, 65);
+  async function initializeApp() {
+    try {
+      // 初始化设置
+      initSetting();
+      let settingData: Record<string, boolean> = await getConfig([
+        "settingConfig",
+      ]);
+      if (!settingData) {
+        settingData = {};
+      }
+      // 监听设置变化,将所有设置都放在main窗口里执行
+      listen(
+        "setting-change",
+        (event: { payload: { key: string; value: boolean } }) => {
+          const { key, value } = event.payload;
+          handleSettingChange(key, value);
+        },
+      );
+      await runStartupTasks((key) => {
+        return settingData[key] === true;
+      });
+    } catch (error) {
+      console.error("初始化失败:", error);
+    }
+  }
 
-//   initializeApp();
-// }
+  initializeApp();
+}
 
 const app = createApp(App);
 app.use(router);
